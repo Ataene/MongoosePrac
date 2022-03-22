52407258d3978ee55f1fb657373852f99787c7f4
Emmanuel Alafonye K <ke.alafonye@yahoo.com>
2022-03-21 19:49:43 -0600

    Added createdAt and email

--- mongoose.js
+++ mongoose.js
@@ -7,17 +7,40 @@ const peopleSchema = new mongoose.Schema({
     name: String,
     title: String,
     age: Number,
+    phone: Number,
+    createdAt: { 
+        type: Date
+    },
+    email: String
 });
 
 const People = mongoose.model("People", peopleSchema);
 
 const people = new People({
 
-    name: "Emmanuel",
-    title: "Engineer",
+    name: "Emmanuel Kpot",
+    title: "Petroleum Engineer",
     age: 33,
+    Phone: 306-339-3978,
+    email: "emmail@gmail.com",
+    createdAt: Date()
+});
+
+const person = new People({
+
+    name: "Ataene Ene",
+    title: "Software Engineer",
+    age: 33,
+    Phone: 306-339-3978,
+    email: "emmail@gmail.com",
+    createdAt: Date()
+
+    
 });
 people.save();
+person.save();
 
 console.log(people);
+console.log(person);
+
 
